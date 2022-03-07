81d57edd99ab3f5da763fa788513567c8dead535
Samrat <samratissteve@gmail.com>
2022-03-07 16:47:08 -0600

    sdfg

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Silm Shirt',
+      slug: 'nike-silm-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', //679*829
+      price: 120,
+      countstock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 120,
+      countstock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+
+    {
+      name: 'Nike Silm Pant',
+      slug: 'nike-silm-pant',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 120,
+      countstock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pant',
+    },
+
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Shirts',
+      image: '/images/p4n.jpg',
+      price: 120,
+      countstock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pant',
+    },
+  ],
+};
+export default data;
